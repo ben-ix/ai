30cefeb996bd4a015c1a4a3d40a681363d2c8e8b
Ben <ben.evans_9@hotmail.com>
2016-04-12 21:48:42 +1200

    Update Feature.java

--- Perceptron/Feature.java
+++ Perceptron/Feature.java
@@ -1,5 +1,3 @@
-package Perceptron;
-
 import java.util.Random;
 
 public class Feature {
@@ -7,7 +5,15 @@ public class Feature {
 	public final int row[];
 	public final int col[];
 	public final boolean sgn[];
-	private double weight;
+	
+	/**
+	 * The dummy feature
+	 */
+	public Feature(){
+		row = null;
+		col = null;
+		sgn = null;
+	}
 	
 	public Feature(int numFeatures){
 		row = new int[numFeatures];
@@ -23,34 +29,38 @@ public class Feature {
 			sgn[i] = r.nextBoolean();
 		}
 		
-		weight = r.nextDouble();
 	}
 	
-	public int evaluate(Image img){
+
+	public Feature(Feature old){
+		this.row = old.row;
+		this.col = old.col;
+		this.sgn = old.sgn;
+	}
+	
+	public int evaluate(boolean[][] img){
+		if(row == null) return 1;
+		
 		int sum = 0;
 		
 		for(int i=0; i<4 ;i++){
-			sum += img.getPixel(row[i], col[i]) == sgn[i] ? 1 : 0;
+			sum += img[row[i]][col[i]] == sgn[i] ? 1 : 0;
 		}
 		
 		return sum >= 3 ? 1 : 0;
 	}
 	
-	public double getWeight(){
-		return weight;
-	}
-	
-	public void incrementWeight(double inc){
-		weight += inc;
-	}
 	
 	@Override
 	public String toString() {
+		if(row == null){
+			return "Dummy feature";
+		}
+		
 		String s = "";
 		for(int i=0; i<sgn.length; i++){
-			s += "("+row[i]+","+col[i]+"): " +sgn[i];
+			s += "("+row[i]+","+col[i]+"):" +sgn[i]+" ";
 		}
-		s+= "\n";
 		return s;
 	}
 	
